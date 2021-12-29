c1bb3533d6394d7bbd5e3e563c01838b386a47ea
hardy-navadiya <hardynavadiya.tech@gmail.com>
2021-12-29 16:57:01 +0530

    finish user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+
+	def gravatar(user)
+		id = Digest::MD5::hexdigest(user.email.downcase)
+		url = "https://secure.gravatar.com/avatar/#{id}"
+		image_tag(url, alt: user.name)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   get 'signup' => 'users#new'
 
   root 'home#index'
+
+  resources :users
 end
